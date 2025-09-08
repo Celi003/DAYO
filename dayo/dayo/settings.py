7a2ba0af675353a0497ff72c0e8e88ad82227489
Celi003 <123420334+Celi003@users.noreply.github.com>
2025-09-08 12:40:09 +0100

    new

--- dayo/dayo/settings.py
+++ dayo/dayo/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-y)f9!)*q@52-e(p)pyhzmggmf@791p3uh^%d$k^rll094t+75h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com', 'dayo.vercel.app']
 
 
 # Application definition
@@ -52,6 +52,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://.onrender.com",
+    "https://dayo.vercel.app"
 ]
 
 CORS_ALLOW_METHODS = [
@@ -124,13 +126,23 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'dayo',
+       'USER': 'dayo_lqpo_user',
+       'PASSWORD': 'Q9iwfIsBpYUO7kZWDwKH9eVQIkb0Lmcr',
+       'HOST': 'dpg-d2vbkan5r7bs73cn37g0-a',
+       'PORT': '5432',  # par défaut pour PostgreSQL
+   }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
